c6507e410bd0e63a8960a12a3fe9a8bc0d51698f
Nathan Bedell <nbedell@tulane.edu>
2022-10-22 15:10:28 -0600

    Add stub for a naive bayes classifier.

--- app/HsNLP/Classifiers.hs
+++ app/HsNLP/Classifiers.hs
@@ -1,3 +1,7 @@
 
 module HsNLP.Classifiers where
 
+newtype Classifier tok cat = Classifier { classify :: [tok] -> cat }
+
+naiveBayes :: Tokenizer String String -> Map cat String -> Classifier String cat
+naiveBayes = undefined
