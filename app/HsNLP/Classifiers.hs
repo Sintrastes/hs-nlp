a551760bff78ef80ac2ba7fe0c91f8fcbf66c1a9
Nathan Bedell <nbedell@tulane.edu>
2022-10-23 09:44:28 -0600

    Add stubs for evaluation functions.

--- app/HsNLP/Classifiers.hs
+++ app/HsNLP/Classifiers.hs
@@ -3,5 +3,24 @@ module HsNLP.Classifiers where
 
 newtype Classifier tok cat = Classifier { classify :: [tok] -> cat }
 
-naiveBayes :: Tokenizer String String -> Map cat String -> Classifier String cat
+naiveBayes :: Tokenizer String String 
+    -> Map cat String 
+    -> Classifier String cat
 naiveBayes = undefined
+
+precision ::
+    Classifier String cat
+ -> Map cat String 
+ -> Double
+precision classifier testData = undefined
+
+recall ::
+    Classifier String cat
+ -> Map cat String 
+ -> Double
+recall classifier testData = undefined
+
+fScore :: Classifier String cat
+ -> Map cat String 
+ -> Double
+fScore classifier testData = undefined
